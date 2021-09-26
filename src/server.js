144eb95d698c94864366f385e689f0e0340a8766
Sunseungcheol <vsoghlv@naver.com>
2021-09-26 21:12:53 +0900

    catchcall url 추가

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
-//public 경로로 접속시 /public를 보여주도록 한다.
+//public 경로로 접속시 유저에게 /public을 보여주도록 한다.
 app.use("/public", express.static(__dirname + "/public"));
 //홈으로 가면 request, response를 받고 home 를 render 해준다.
 app.get("/", (req, res) => res.render("home"));
+//catchcall url, 유저가 어느 경로로 이동하든 /home 으로 보내준다.
+app.get("/*", (req, res) => res.render("/home"));
 
 app.listen(3000);
 
